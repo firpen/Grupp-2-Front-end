daad45b54ff901cc6038303e44e1131aed2df6c7
Robert <boren534@gmail.com>
2026-02-10 15:27:06 +0100

    byggt första fetch med X-API-KEY

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+var apiKey = "9340c3cf-7a20-48a1-8bc2-f5e7ee6de981"
+var carsContainer = document.getElementById("carsContainer");
+
+
+function getCars() {
+    fetch("http://localhost:8080/api/cars", {
+        headers: {
+            "Content-Type": "application/json",
+            "X-API-KEY": apiKey
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        carsContainer.innerHTML = ""; // rensa
+
+        data.forEach(car => {
+            let div = document.createElement("div");
+
+            div.innerHTML = `
+                <h2>${car.carBrand} ${car.carModel}</h2>
+                <img src="${car.img}" width="200"/>
+                <p><strong>Year:</strong> ${car.year}</p>
+                <p><strong>Value:</strong> ${car.value}</p>
+                <p><strong>Brand:</strong> ${car.carBrand}</p>
+                <p><strong>Model:</strong> ${car.carModel}</p>
+            `;
+
+            carsContainer.appendChild(div);
+        });
+    })
+    .catch(error => console.error("Error fetching:", error));
+}
+
+getCars();
